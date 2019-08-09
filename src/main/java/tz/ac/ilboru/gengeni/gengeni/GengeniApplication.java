1392dc9e5e7416687d11a3fa27e7ceed902b4cef
Boniface Chacha <bonifacechacha@gmail.com>
2019-08-09 19:39:59 +0100

    having fun

---
+++ src/main/java/tz/ac/ilboru/gengeni/gengeni/GengeniApplication.java
@@ -0,0 +1,13 @@
+package tz.ac.ilboru.gengeni.gengeni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GengeniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GengeniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tz/ac/ilboru/gengeni/gengeni/GengeniApplicationTests.java
@@ -0,0 +1,16 @@
+package tz.ac.ilboru.gengeni.gengeni;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GengeniApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
